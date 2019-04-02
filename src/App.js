475684d7a2ec857db52ef3d7733cc3a22adc613a
Natalia PN <npelegero@gmail.com>
2019-04-02 13:12:33 +0200

    Add main section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import user from './images/user.svg';
 import bag from './images/bag.svg';
+import products from './images/products.png';
+import vans_36 from './images/vans-36.png';
+
+
 
 import './App.css';
 
@@ -9,10 +13,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={user} className="User-account" alt="user account"/>
+          <img src={user} className="User-account" alt="user account"></img>
 
-          <img src={bag} className="Shopping-bag" alt="shopping bag"/>
+          <img src={bag} className="Shopping-bag" alt="shopping bag"></img>
         </header>
+
+        <main className="Main-section">
+          <img className="Products-display" src={products} alt="products display"></img>
+
+          <div className="Open-bag__action-container">
+            <img className="Open-bag__trigger" src={bag} alt="shopping bag"></img>
+
+            <p className="Open-bag_call">Open bag</p>
+          </div>
+        </main> 
       </div>
     );
   }
