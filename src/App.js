9042f33e158c7cff4dde5103a472f002f70da0c2
Natalia PN <npelegero@gmail.com>
2019-04-02 14:07:56 +0200

    Add payment section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import products from './images/products.png';
 import vans_36 from './images/vans-36.png';
 import vans_44 from './images/vans-44.png';
 import vans_39 from './images/vans-39.png';
+import down from './images/down-arrow.svg';
+
 
 
 
@@ -46,7 +48,7 @@ class App extends Component {
                     <div className="Delete-action__container">
                       <button type="button" className="Delete-action__button"><i className="far fa-trash-alt"></i></button>
 
-                      <p className="Delete-action__line">Eliminar</p>
+                      <p className="Delete-action">Eliminar</p>
                     </div>
 
                     <p className="Item__size">Talla: 36</p>
@@ -70,7 +72,7 @@ class App extends Component {
                     <div className="Delete-action__container">
                       <button type="button" className="Delete-action__button"><i className="far fa-trash-alt"></i></button>
 
-                      <p className="Delete-action__line">Eliminar</p>
+                      <p className="Delete-action">Eliminar</p>
                     </div>
 
                     <p className="Item__size">Talla: 44</p>
@@ -96,7 +98,7 @@ class App extends Component {
                     <div className="Delete-action__container">
                       <button type="button" className="Delete-action__button"><i className="far fa-trash-alt"></i></button>
 
-                      <p className="Delete-action__line">Eliminar</p>
+                      <p className="Delete-action">Eliminar</p>
                     </div>
 
                     <p className="Item__size">Talla: 39</p>
@@ -109,6 +111,28 @@ class App extends Component {
                       <div className="Quantity-decrease__box">-</div>
                     </div>
                   </div> 
+
+                  <div className="Promotional-code__container">
+                    <p className="Promotional-code">Código promocional</p>
+                    <img className="Promotional-code__selection" src={down} alt="selection arrow"></img>
+                  </div>
+
+                  <div className="Subtotal__container">
+                    <p className="Subtotal">Subtotal</p>
+                    <p className="Subtotal__amount">xxx€</p>
+                  </div>
+
+                  <div className="Total__container">
+                    <p className="Total">Total</p>
+                    <p className="Total__amount">xxx€</p>
+                  </div>
+
+                  <div className="Purchase-button__container">
+                    <button type="button" className="Purchase-button">comprar</button>
+               
+                  </div>
+
+
                 </div>
               </div>
             </div>
