e5d613ce40c32c96fe7fc1b69757720929f54daa
Natalia PN <npelegero@gmail.com>
2019-04-10 12:36:22 +0200

    Add docs folder

---
+++ docs/precache-manifest.06c83b1a8a55577b01408d9480b92abf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "70611ce51156a95a30d362e0be05746d",
+    "url": "./static/media/products-display.70611ce5.png"
+  },
+  {
+    "revision": "af5f27685f3b45f3adb8",
+    "url": "./static/css/main.8d94cc6d.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "599a16f0e1209c5daba9",
+    "url": "./static/js/2.71f9aca4.chunk.js"
+  },
+  {
+    "revision": "580a06416f7feb75ff297df09b2b0434",
+    "url": "./static/media/user.580a0641.svg"
+  },
+  {
+    "revision": "6ad1cec592c99b0499703a3a0de4ea63",
+    "url": "./static/media/bag.6ad1cec5.svg"
+  },
+  {
+    "revision": "af5f27685f3b45f3adb8",
+    "url": "./static/js/main.4bc5e13e.chunk.js"
+  },
+  {
+    "revision": "7b4a030d93c5c601a93a12cb5e93e901",
+    "url": "./static/media/vans-36.7b4a030d.png"
+  },
+  {
+    "revision": "82789701915e8d79e02eea9b81627e4b",
+    "url": "./static/media/vans-44.82789701.png"
+  },
+  {
+    "revision": "342badf645850a6a3ce30ec752424ba6",
+    "url": "./static/media/vans-39.342badf6.png"
+  },
+  {
+    "revision": "9edb5402534d814628d4369e8fb54f7e",
+    "url": "./static/media/down-arrow.9edb5402.svg"
+  },
+  {
+    "revision": "3de99deefddbb4ffa22debc929bce500",
+    "url": "./index.html"
+  }
+];
